40203b855a9eca93e6f875a4a0f2f0797c74403f
csmate79 <36413556+csmate79@users.noreply.github.com>
2018-02-14 08:28:28 +0100

    Add files via upload

---
+++ valtcsere.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a=2000;
+  int b=28;
+
+    cout<< a <<" "<< b <<endl;
+
+  a=b-a;
+  b=b-a;
+  a=b+a
+
+    cout<< a <<" "<< b <<endl;
+
+  a=b-a;
+  b=b-a;
+  a=b+a;
+
+    cout<< a <<" "<< b <<endl;
+
+return 0;
+
+}
