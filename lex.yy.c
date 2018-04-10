b9af033cbd531ea4a7a557548b742aff2083ee84
csmate79 <36413556+csmate79@users.noreply.github.com>
2018-04-10 12:54:58 +0200

    Add files via upload

---
+++ lex.yy.c
@@ -0,0 +1,36 @@
+%{
+#include<stdio.h>
+int sorok=0, szavak=0,betuk=0, szam=0,total=0;
+%}
+%%
+
+
+ 
+\n { sorok++; szavak++;}
+[\t ' '] szavak++;
+[A-Z] betuk++;
+[a-z] betuk++;
+[0-9] szam++;
+%%
+
+
+
+
+
+main(void)
+{
+yyin= fopen("myfile.txt","r");
+yylex();
+total=betu+szam;
+printf(" A fajl tartalmaz ...");
+printf("\n\t%d sort.", sorok);
+printf("\n\t%d szot.",szavak);
+printf("\n\t%d betut.", betuk);
+printf("\n\t%d szamot.", szam);
+printf("\n\taz osszes %d karakterben.\n",total);
+}
+ 
+int yywrap()
+{
+return(1);
+}
